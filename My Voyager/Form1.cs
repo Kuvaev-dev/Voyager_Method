d3e7a038dcbe1e6c9d0ccb17c5401f68199fc3f1
Somewhere <66011798+Somwere@users.noreply.github.com>
2021-03-14 02:16:38 +0200

    Form Changes

--- My Voyager/Form1.cs
+++ My Voyager/Form1.cs
@@ -7,7 +7,7 @@ namespace My_Voyager
 {
     public partial class Form1 : Form
     {
-       
+
         public Form1()
         {
             InitializeComponent();
@@ -15,7 +15,7 @@ namespace My_Voyager
 
         public void Form1_Load(object sender, EventArgs e)
         {
-            
+
 
         }
         public int c;
@@ -24,7 +24,7 @@ namespace My_Voyager
         {
             // 1. Инициализация и заполнение матрицы
             myMatrix.Enabled = true;
-            
+
             info.Text = "Step 1. Заполнение матрицы городами.\nПоле с матрицей заполняется городами по главной диагонали. Количество городов введено пользователем. Заполните значения матрицы вручную. Максимальное количество городов - 10.";
             c = Convert.ToInt32(counter.Text);
             if (c > 10)
@@ -36,6 +36,7 @@ namespace My_Voyager
             Random rand = new Random();
             myMatrix.ColumnCount = c;
             myMatrix.RowCount = c;
+            
             for (int i = 0; i < c; i++)
             {
                 for (int j = 0; j < c; j++)
@@ -53,7 +54,7 @@ namespace My_Voyager
                     myMatrix.Rows[i].Cells[i].Style.ForeColor = Color.White;
                     myMatrix.Rows[i].Cells[i].Value = 0;
                     myMatrix.Rows[i].Cells[i].ReadOnly = true;
-                    
+
                 }
             }
         }
@@ -62,28 +63,28 @@ namespace My_Voyager
         {
             // 1. Внесение коллекции
 
-            List<TextBox> lst = new List<TextBox>()
+            List<Label> lst = new List<Label>()
             {
-                new TextBox() {Location = new Point(502, 119), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(502, 145), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(502, 171), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(502, 197), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(502, 223), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(502, 249), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(502, 275), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(502, 301), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(502, 327), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(502, 353), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(107, 407), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(141, 407), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(175, 407), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(209, 407), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(243, 407), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(278, 407), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(312, 407), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(346, 407), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(380, 407), Size = new Size(28, 20)},
-                new TextBox() {Location = new Point(73, 407), Size = new Size(28, 20)},
+                new Label() {Location = new Point(502, 119), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(502, 145), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(502, 171), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(502, 197), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(502, 223), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(502, 249), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(502, 275), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(502, 301), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(502, 327), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(502, 353), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(73, 407), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(107, 407), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(141, 407), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(175, 407), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(209, 407), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(243, 407), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(278, 407), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(312, 407), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(346, 407), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
+                new Label() {Location = new Point(380, 407), Size = new Size(28, 20), BackColor = Color.White, ForeColor = Color.Black},
             };
 
             foreach (var item in lst)
@@ -97,39 +98,76 @@ namespace My_Voyager
             for (int i = 0; i < c; i++)
             {
                 int min = 0;
+                
+
                 for (int j = 0; j < c; j++)
                 {
-                    if (j == 0)
-                        min = (int)myMatrix.Rows[i].Cells[j].Value;
-                    if ((int)myMatrix.Rows[i].Cells[j].Value < min)
-                        min = (int)myMatrix.Rows[i].Cells[j].Value;
-                }
 
-                if (i == 0)
-                    lst[0].Text = min.ToString();
-                if (i == 1)
-                    lst[1].Text = min.ToString();
-                if (i == 2)
-                    lst[2].Text = min.ToString();
-                if (i == 3)
-                    lst[3].Text = min.ToString();
-                if (i == 4)
-                    lst[4].Text = min.ToString();
-                if (i == 5)
-                    lst[5].Text = min.ToString();
-                if (i == 6)
-                    lst[6].Text = min.ToString();
-                if (i == 7)
-                    lst[7].Text = min.ToString();
-                if (i == 8)
-                    lst[8].Text = min.ToString();
-                if (i == 9)
-                    lst[9].Text = min.ToString();
-                if (i == 10)
-                    lst[10].Text = min.ToString();
+                    if ((int)myMatrix.Rows[i].Cells[j].Value != 0)
+                    {
+                        if (j == 0)
+                            min = (int)myMatrix.Rows[i].Cells[j].Value;
+
+                        if ((int)myMatrix.Rows[i].Cells[j].Value < min)
+                            min = (int)myMatrix.Rows[i].Cells[j].Value;
+
+
+                        if (i == 0)
+                            lst[0].Text = min.ToString();
+                        if (i == 1)
+                            lst[1].Text = min.ToString();
+                        if (i == 2)
+                            lst[2].Text = min.ToString();
+                        if (i == 3)
+                            lst[3].Text = min.ToString();
+                        if (i == 4)
+                            lst[4].Text = min.ToString();
+                        if (i == 5)
+                            lst[5].Text = min.ToString();
+                        if (i == 6)
+                            lst[6].Text = min.ToString();
+                        if (i == 7)
+                            lst[7].Text = min.ToString();
+                        if (i == 8)
+                            lst[8].Text = min.ToString();
+                        if (i == 9)
+                            lst[9].Text = min.ToString();
+
+                    }
+                    if((int)myMatrix.Rows[j].Cells[i].Value != 0)
+                    {
+                        int min2 = 0;
+                        if (i == 0)
+                            min2 = int.Parse(myMatrix.Rows[i].Cells[j].Value.ToString());
+                    
+                        if (min2 > int.Parse(myMatrix.Rows[i].Cells[j].Value.ToString()))
+                            min2 = int.Parse(myMatrix.Rows[i].Cells[j].Value.ToString());
+                        
+                        if (j == 0)
+                            lst[10].Text = min2.ToString();
+                        if (j == 1)
+                            lst[11].Text = min2.ToString();
+                        if (j == 2)
+                            lst[12].Text = min2.ToString();
+                        if (j == 3)
+                            lst[13].Text = min2.ToString();
+                        if (j == 4)
+                            lst[14].Text = min2.ToString();
+                        if (j == 5)
+                            lst[15].Text = min2.ToString();
+                        if (j == 6)
+                            lst[16].Text = min2.ToString();
+                        if (j == 7)
+                            lst[17].Text = min2.ToString();
+                        if (j == 8)
+                            lst[18].Text = min2.ToString();
+                        if (j == 9)
+                            lst[19].Text = min2.ToString();
+                    }
+                    
+                }
             }
-           
+            
         }
-        
     }
 }
